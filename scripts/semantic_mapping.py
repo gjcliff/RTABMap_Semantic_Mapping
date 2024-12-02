a57b2c7d575b6c518891a74105e8173fa600c54d
Graham Clifford <gjcliff@gmail.com>
2024-12-01 22:01:19 -0600

    cleaned up old files that weren't being used

--- scripts/semantic_mapping.py
+++
@@ -1,7 +0,0 @@
-import database_exporter_py
-from sensor_msgs.msg import PointCloud2
-import cv2 as cv
-
-exporter = database_exporter_py.DatabaseExporter('lab-20241112.db', '')
-result = exporter.load_rtabmap_db()
-print(f"Loaded {len(result.images)} images, {len(result.depths)} depths and {len(result.pointcloud)} points")
